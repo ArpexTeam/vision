fc65ef0f66655017e3d54f4314f1cdbd25340f6a
davidonedev <david.amc18@gmail.com>
2025-03-03 10:51:18 -0300

    Alterações page comercial 03/03

--- src/components/CardCommercial/index.jsx
+++ src/components/CardCommercial/index.jsx
@@ -11,7 +11,7 @@ function CardCommercial(props){
     const [visible, setVisible] = useState(false);
     return(
         
-        <div className={`${props.type == 'large' ? 'h-56 w-[31%]' : 'h-[350px] w-[25%]'} bg-gray-700 mt-6`} style={{backgroundImage:`url(${props.thumb})`, backgroundSize:'cover', backgroundPosition:'center'}}>
+        <div className={`${props.type == 'large' ? 'h-56 w-[31%]' : 'h-[350px] w-[30%]'} bg-gray-700 mt-6`} style={{backgroundImage:`url(${props.thumb})`, backgroundSize:'cover', backgroundPosition:'center'}}>
             <ModalPic type={props.typeModal} close={openClose} thumb={props.video} visibleProps={visible}/>
 
             <button onClick={openClose} style={{border:'1px solid #06E7F2'}} className='hover:bg-transparent hover:text-[#06E7F2] font-[ClashDisplay-Medium] ml-auto mr-auto bg-[#06E5F1] relative -bottom-[95%] px-8 py-2 rounded-md'>View more</button>
