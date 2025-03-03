a41792faff2e8dcd3786c3dfe58236aabbbfe7de
davidonedev <david.amc18@gmail.com>
2025-03-03 10:51:18 -0300

    Alterações page comercial 03/03

--- src/Pages/Commercial/index.jsx
+++ src/Pages/Commercial/index.jsx
@@ -24,20 +24,20 @@ function Commercial(){
         <div className='bg-black w-full pt-20 h-auto relative'>
             <div className='max-w-[1280px] w-4/5 mr-auto ml-auto h-auto relative'>  
             <div className='items-center justify-center flex'>
-            <div className='text-left'>
-                <h1 className="font-[ClashDisplay-Bold] text-[36px] titleBlueLine">CREATIVE STRATEGIES TO BOOST YOUR SALES</h1>
-                <p className='text-[#CCCCCC] font-[ClashDisplay-Regular]'>Optimizes your sales, better engagement, and top business results.</p>
+            <div className='text-left w-[1200px]'>
+                <h1 className="font-[ClashDisplay-SemiBold] text-[36px] titleBlueLine">CREATIVE STRATEGIES TO BOOST YOUR SALES</h1>
+                <p className='text-[#CCCCCC] font-[ClashDisplay-Regular]'>Optimizes your sales, better engagement, <br /> and top business results.</p>
             </div>
             <div>
-                <img src={commercial} className='w-7/12 ml-auto'/>
+                <img src={commercial} className='w-8/12 ml-auto'/>
             </div>
             </div>
 
             <div>
-                <h1 className='font-[ClashDisplay-Bold] text-[40px] mt-36'>Commercial Portfolio</h1>
-                <p className='text-[#CCCCCC]'>Working with the best clients and collaborators.</p>
+                <h1 className='font-[ClashDisplay-SemiBold] text-[40px] mt-36'>Commercial Portfolio</h1>
+                <p className='text-[#CCCCCC] font-[ClashDisplay-regular]'>Working with the best clients and collaborators.</p>
             </div>
-            <div className='flex gap-6 justify-between flex-wrap mt-24'>
+            <div className='flex gap-6 justify-between flex-wrap mt-10'>
             <CardCommercial type="large" thumb={cm1} video={video1} typeModal="video"/>
             <CardCommercial type="large" thumb={cm2}/>
             <CardCommercial type="large" thumb={cm3}/>
@@ -47,10 +47,10 @@ function Commercial(){
             </div>
 
             <div>
-                <h1 className='font-[ClashDisplay-Bold] text-[40px] mt-36'>Social Media Manegement</h1>
-                <p className='text-[#CCCCCC]'>Let a professional production studio take care of your business!</p>
+                <h1 className='font-[ClashDisplay-SemiBold] text-[40px] mt-36'>Social Media Manegement</h1>
+                <p className='text-[#CCCCCC] font-[ClashDisplay-Regular]'>Let a professional production studio take care of your business!</p>
             </div>
-            <div className='flex gap-10 justify-between flex-wrap mt-24'>
+            <div className='flex gap-10 justify-between flex-wrap mt-10'>
             <CardCommercial type="high" thumb={sm1} video={pic1} typeModal="pic"/>
             <CardCommercial type="high" thumb={sm2}/>
             <CardCommercial type="high" thumb={sm3}/>
@@ -59,9 +59,9 @@ function Commercial(){
             <CardCommercial type="high" thumb={sm6}/>
             </div>
             </div>
-            <div className="w-full py-20 bg-[#070707] mt-56">
+            <div className="w-full py-10 bg-[#0e0e0e] mt-36">
                 <div className="max-w-[1280px] ml-auto mr-auto flex justify-center text-center w-4/5 flex-col items-center">
-                <h1 className="font-[ClashDisplay-Semibold] text-[26px] w-2/3">Speak with one of our specialists and receive a personalized quote right now</h1>
+                <h1 className="font-[ClashDisplay-Medium] text-[26px] w-2/3">Speak with one of our specialists and receive a personalized quote right now</h1>
                 <button className="mt-10 bg-[#06E7F2] h-fit w-fit p-3 px-14 rounded-xl font-[ClashDisplay-Semibold] hover:bg-transparent hover:text-[#06E7F2]" style={{border:'1px solid #06E7F2'}}>Contact us</button>
                 </div>
             </div>
