189b0ef80fc56aceb293f4f6c0fad09cc4bc229f
davidonedev <david.amc18@gmail.com>
2025-03-10 21:26:40 -0300

    correçoes bugs mobile

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -38,14 +38,14 @@ function Services(){
     }
 
     return(
-        <div className='bg-black w-full py-20 h-auto relative'>
+        <div className='bg-black w-full md:py-20 h-auto relative'>
             <div className='max-w-[1280px] w-4/5 mr-auto ml-auto h-auto relative'>
-                <h1 className='text-[36px] font-[ClashDisplay-Semibold]'>Hard to sell? we are the<br/> solution!</h1>
+                <h1 className='text-[28px] md:text-[36] font-[ClashDisplay-Semibold]'>Hard to sell? we are the<br/> solution!</h1>
 
-                <div className='w-full mt-14 mb-36 lg:mb-0'>
-                    <div className='flex flex-col lg:flex-row w-10/12 h-[450px] lg:h-[350px] items-center lg:items-start lg:justify-between gap-2 xl:gap-5 transition-all servicesCard relative ml-auto mr-auto'>
+                <div className='w-full mt-14 mb-20 lg:mb-0'>
+                    <div className='flex flex-col lg:flex-row w-10/12 h-[550px] lg:h-[350px] items-center lg:items-start lg:justify-between gap-2 xl:gap-5 transition-all servicesCard relative ml-auto mr-auto'>
                     <div className='text-center lg:text-left w-10/12 xl:w-4/12 mb-10 lg:mb-0'>
-                        <h1 className='text-[38px] font-[ClashDisplay-Semibold] titleBlueLine mt-8'>COMMERCIAL</h1>
+                        <h1 className='text-[24px] md:text-[38px] font-[ClashDisplay-Semibold] titleBlueLine mt-8 mb-4'>COMMERCIAL</h1>
                         <p className='text-[#A5A5A5] font-[ClashDisplay-medium]'>Boost your sales with commercials
                             that captivate and deliver results.
                             Let’s create together!
@@ -63,7 +63,7 @@ function Services(){
                             </div>
                             </div>
                             <video
-                                    className="w-full h-[300px] object-cover border-4 border-[#000] rounded-2xl"
+                                    className="w-full h-full object-cover border-4 border-[#000] rounded-2xl"
                                     controls
                                     ref={videoRef}
                                     onPause={pauseResume}
@@ -78,21 +78,21 @@ function Services(){
                     </div>
                     </div>
 
-                    <h1 className='text-[36px] font-[ClashDisplay-Semibold] mt-10 mb-20'>Elevate your event to the<br/> highest level!</h1>
+                    <h1 className='text-[28px] md:text-[36px] font-[ClashDisplay-Semibold] mb-16'>Elevate your event to the<br/> highest level!</h1>
 
 
                     <div className='w-full mt-14'>
 
-                    <div className='flex flex-col lg:flex-row w-10/12 h-[450px] lg:h-[350px] items-center lg:items-start lg:justify-between gap-2 xl:gap-5 transition-all servicesCard relative ml-auto mr-auto'>
+                    <div className='flex flex-col lg:flex-row w-10/12 h-[550px] lg:h-[350px] items-center lg:items-start lg:justify-between gap-2 xl:gap-5 transition-all servicesCard relative ml-auto mr-auto'>
                     <div className='w-12/12 xl:w-7/12 h-full rounded-2xl'>
                     <div className='w-full h-full relative'>
                             <div onClick={pauseResume2} className={`${isPlaying2 ? "hidden" : "block"} absolute w-full h-full z-10 bg-[#000000]/50 flex justify-center items-center cursor-pointer`}>
-                            <div className="bg-[#06E5F1] w-fit h-fit px-7 py-2 rounded-lg mb-20">
+                            <div className="bg-[#06E5F1] w-fit h-fit px-7 py-2 rounded-lg mb-10">
                                 <img src={rightArrow} className="w-4"/>
                             </div>
                             </div>
                             <video
-                                    className="w-full h-[300px] object-cover border-4 border-[#000] rounded-2xl"
+                                    className="w-full h-full object-cover border-4 border-[#000000] rounded-2xl mb-16"
                                     controls
                                     ref={videoRef2}
                                     onPause={pauseResume2}
@@ -102,14 +102,14 @@ function Services(){
                                 </video>
                         </div>
                     </div>
-                    <div className='text-center lg:text-left w-10/12 xl:w-4/12 mb-10 lg:mb-0'>
-                        <h1 className='text-[38px] font-[ClashDisplay-Semibold] titleBlueLine mt-8'>EVENTS</h1>
+                    <div className='text-center lg:text-left w-10/12 box-border block xl:w-4/12 mb-10 lg:mb-0 '>
+                        <h1 className='text-[24px] md:text-[38px] font-[ClashDisplay-Semibold] titleBlueLine mt-8 mb-4'>EVENTS</h1>
                         <p className='text-[#A5A5A5] font-[ClashDisplay-medium]'>
                             Transform your event with dynamic
                             visuals and storytelling
                             that captivate your audience.
                         </p>
-                        <button className='bg-[#06E5F1] hover:bg-transparent hover:text-[#06E5F1] hover:border-2 hover:border-[#06E5F1] px-7 py-3 mt-10 rounded-md font-[ClashDisplay-Medium] transition-all duration-150 ease-in-out' style={{border:'1px solid #06E7F2'}}>
+                        <button className='bg-[#06E5F1] hover:bg-transparent hover:text-[#06E5F1] hover:border-2 hover:border-[#06E5F1] px-7 py-3 mt-10 mb-10 rounded-md font-[ClashDisplay-Medium] transition-all duration-150 ease-in-out' style={{border:'1px solid #06E7F2'}}>
                             View more
                         </button>
                     </div>
